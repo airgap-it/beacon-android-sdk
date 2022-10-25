95860d171a2fd4ed6ed90958d7cc0df953bbc0ea
Julia Samol <j.samol@papers.ch>
2022-10-25 16:39:20 +0200

    chore(): update target SDK

--- buildSrc/src/main/java/GradleConfig.kt
+++ buildSrc/src/main/java/GradleConfig.kt
@@ -1,7 +1,7 @@
 object Android {
-    const val compileSdk = 32
+    const val compileSdk = 33
     const val minSdk = 21
-    const val targetSdk = 32
+    const val targetSdk = 33
 
     const val versionCode = 27
     const val versionName = "3.2.2"
